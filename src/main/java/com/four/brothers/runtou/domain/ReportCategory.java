fec6aaf523340a4ba24c622c71e65999b668ae72
TaegyunWoo <76861101+TaegyunWoo@users.noreply.github.com>
2022-01-29 21:44:57 +0900

    Feat: ENUM value 필드 제어자 추가
    상수 값이므로 final 을 추가함.

--- src/main/java/com/four/brothers/runtou/domain/ReportCategory.java
+++ src/main/java/com/four/brothers/runtou/domain/ReportCategory.java
@@ -4,7 +4,7 @@ public enum ReportCategory {
   PROCESS_TIME("수행시간"), PROCESS_CONTEXT("수행내용"),
   BAD_WORD("욕설"), SEXUAL_HARASSMENT("성희롱");
 
-  private String value;
+  private final String value;
 
   ReportCategory(String value) {
     this.value = value;
