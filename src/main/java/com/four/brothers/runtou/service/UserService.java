ea54730df086f53ae2e58acea020a805811dc52b
TaegyunWoo <76861101+TaegyunWoo@users.noreply.github.com>
2022-03-06 14:20:57 +0900

    Feat: 로그인 서비스 추가

--- src/main/java/com/four/brothers/runtou/service/UserService.java
+++ src/main/java/com/four/brothers/runtou/service/UserService.java
@@ -1,9 +1,14 @@
 package com.four.brothers.runtou.service;
 
+import com.four.brothers.runtou.domain.Orderer;
+import com.four.brothers.runtou.domain.Performer;
 import com.four.brothers.runtou.domain.User;
+import com.four.brothers.runtou.dto.LoginDto;
 import com.four.brothers.runtou.dto.OrdererDto;
 import com.four.brothers.runtou.dto.UserDto;
+import com.four.brothers.runtou.dto.UserRole;
 import com.four.brothers.runtou.repository.user.OrdererRepository;
+import com.four.brothers.runtou.repository.user.PerformerRepository;
 import com.four.brothers.runtou.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,11 +17,15 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+import static com.four.brothers.runtou.dto.LoginDto.*;
+import static com.four.brothers.runtou.dto.UserDto.*;
+
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
   private final OrdererRepository ordererRepository;
+  private final PerformerRepository performerRepository;
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
 
@@ -46,9 +55,9 @@ public class UserService {
    * @return
    */
   @Transactional
-  public UserDto.DuplicatedAccountIdResponse isDuplicatedAccountId(UserDto.DuplicatedAccountIdRequest request) {
+  public DuplicatedAccountIdResponse isDuplicatedAccountId(DuplicatedAccountIdRequest request) {
     Optional<User> userByAccountId = userRepository.findUserByAccountId(request.getAccountId());
-    return new UserDto.DuplicatedAccountIdResponse(userByAccountId.isPresent());
+    return new DuplicatedAccountIdResponse(userByAccountId.isPresent());
   }
 
   /**
@@ -59,9 +68,55 @@ public class UserService {
    * @return
    */
   @Transactional
-  public UserDto.DuplicatedNicknameResponse isDuplicatedNickname(UserDto.DuplicatedNicknameRequest request) {
+  public DuplicatedNicknameResponse isDuplicatedNickname(DuplicatedNicknameRequest request) {
     Optional<User> userByNickname = userRepository.findUserByNickname(request.getNickname());
-    return new UserDto.DuplicatedNicknameResponse(userByNickname.isPresent());
+    return new DuplicatedNicknameResponse(userByNickname.isPresent());
+  }
+
+  /**
+   * Orderer로 로그인하는 메서드
+   * @param request
+   * @return
+   */
+  @Transactional
+  public LoginUser loginAsOrderer(LoginRequest request) {
+    String accountId = request.getAccountId();
+    String rawPassword = request.getRawPassword();
+    Optional<Orderer> orderer = ordererRepository.findOrdererByAccountId(accountId);
+    String encodedPassword = orderer.get().getPassword();
+
+    if (orderer.isEmpty() || !passwordEncoder.matches(rawPassword, encodedPassword)) {
+      throw new IllegalArgumentException("로그인 정보가 잘못되었습니다.");
+    }
+
+    return new LoginUser(orderer.get().getAccountId(),
+            orderer.get().getNickname(),
+            orderer.get().getPhoneNumber(),
+            orderer.get().getAccountNumber(),
+            UserRole.ORDERER);
+  }
+
+  /**
+   * Performer로 로그인하는 메서드
+   * @param request
+   * @return
+   */
+  @Transactional
+  public LoginUser loginAsPerformer(LoginRequest request) {
+    String accountId = request.getAccountId();
+    String rawPassword = request.getRawPassword();
+    Optional<Performer> performer = performerRepository.findPerformerByAccountId(accountId);
+    String encodedPassword = performer.get().getPassword();
+
+    if (performer.isEmpty() || !passwordEncoder.matches(rawPassword, encodedPassword)) {
+      throw new IllegalArgumentException("로그인 정보가 잘못되었습니다.");
+    }
+
+    return new LoginUser(performer.get().getAccountId(),
+      performer.get().getNickname(),
+      performer.get().getPhoneNumber(),
+      performer.get().getAccountNumber(),
+      UserRole.PERFORMER);
   }
 
 }
