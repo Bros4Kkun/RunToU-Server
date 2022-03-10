c1e4b7f8d590495dbe1a6699ed5cd710bd789202
TaegyunWoo <76861101+TaegyunWoo@users.noreply.github.com>
2022-03-10 22:16:59 +0900

    Fix: 예외코드 enum 상속 추가

--- src/main/java/com/four/brothers/runtou/exception/code/RequestExceptionCode.java
+++ src/main/java/com/four/brothers/runtou/exception/code/RequestExceptionCode.java
@@ -1,14 +1,16 @@
 package com.four.brothers.runtou.exception.code;
 
-import lombok.Getter;
-
-@Getter
-public enum RequestExceptionCode {
+public enum RequestExceptionCode implements ExceptionCode {
   WRONG_FORMAT("요청시 작성된 json의 형식이나 값이 잘못되었습니다.");
 
   private String value;
 
   RequestExceptionCode(String value) {
     this.value = value;
+  }
+
+  @Override
+  public String getValue() {
+    return this.value;
   }
 }
