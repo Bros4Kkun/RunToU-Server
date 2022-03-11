2112dda0e47e34dfb9f064a97f2312af10f21331
TaegyunWoo <76861101+TaegyunWoo@users.noreply.github.com>
2022-03-11 17:48:26 +0900

    fix #28 - BadRequestException 클래스 생성자 예외 추가
    BadRequestException 객체 생성 시, 정확한 타입을 매개변수로 받기 위해 수정함.

--- src/main/java/com/four/brothers/runtou/exception/BadRequestException.java
+++ src/main/java/com/four/brothers/runtou/exception/BadRequestException.java
@@ -1,11 +1,13 @@
 package com.four.brothers.runtou.exception;
 
 import com.four.brothers.runtou.exception.code.ExceptionCode;
-import com.four.brothers.runtou.exception.code.RequestExceptionCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+/**
+ * "404 Bad Request"를 반환해야 하는 예외
+ */
 @Getter
 @NoArgsConstructor
 public class BadRequestException extends IllegalArgumentException {
@@ -14,7 +16,18 @@ public class BadRequestException extends IllegalArgumentException {
   private ExceptionCode code;
   private String errorMsg;
 
+  /**
+   * ExceptionCode 인터페이스를 상속받은 ENUM 타입 처리
+   * @param code
+   */
   public BadRequestException(ExceptionCode code) {
+    if (!code.getClass().isEnum()) {
+      throw new IllegalArgumentException("매개변수로 enum 타입이 와야합니다.");
+    }
+    if (!(code instanceof ExceptionCode)) {
+      throw new IllegalArgumentException("오류코드 enum은 ExceptionCode 인터페이스를 상속받아야 합니다.");
+    }
+
     this.code = code;
     this.errorMsg = code.getValue();
   }
