12a102aac0d4938de90979ef318116cfc457df63
TaegyunWoo <76861101+TaegyunWoo@users.noreply.github.com>
2022-03-21 21:34:30 +0900

    Feat: OrderSheet 저장 메서드 추가
    OrderSheetRepository에 누락되어있던 save 메서드를 추가함.

--- src/main/java/com/four/brothers/runtou/repository/user/OrderSheetRepository.java
+++ src/main/java/com/four/brothers/runtou/repository/user/OrderSheetRepository.java
@@ -1,16 +1,27 @@
 package com.four.brothers.runtou.repository.user;
 
 import com.four.brothers.runtou.domain.OrderSheet;
+import com.four.brothers.runtou.domain.OrderSheetCategory;
+import com.four.brothers.runtou.domain.Orderer;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public class OrderSheetRepository {
   @PersistenceContext
   EntityManager em;
+
+  public void saveOrderSheet(Orderer orderer, String title, String content,
+                             OrderSheetCategory category, String destination,
+                             int cost, boolean isPayed, LocalDateTime wishedDeadline) {
+    OrderSheet orderSheet = new OrderSheet(orderer, title, content, category, destination,
+                                            cost, isPayed, wishedDeadline);
+    em.persist(orderSheet);
+  }
 
   /**
    * 모든 주문서를 조회하는 메서드
