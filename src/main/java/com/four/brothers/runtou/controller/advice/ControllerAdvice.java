d68fdb4704e6eee6c5da510e39089d29c45dcde0
TaegyunWoo <76861101+TaegyunWoo@users.noreply.github.com>
2022-03-30 20:05:44 +0900

    Feat: 권한 관련 예외 추가

--- src/main/java/com/four/brothers/runtou/controller/advice/ControllerAdvice.java
+++ src/main/java/com/four/brothers/runtou/controller/advice/ControllerAdvice.java
@@ -2,6 +2,7 @@ package com.four.brothers.runtou.controller.advice;
 
 import com.four.brothers.runtou.dto.ErrorDto;
 import com.four.brothers.runtou.exception.BadRequestException;
+import com.four.brothers.runtou.exception.NoAuthorityException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,6 +13,12 @@ public class ControllerAdvice {
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   @ExceptionHandler(IllegalArgumentException.class)
   public ErrorDto.Response badRequestHandle(BadRequestException e) {
+    return new ErrorDto.Response(BadRequestException.httpStatus, e.getCode(), e.getErrorMsg());
+  }
+
+  @ResponseStatus(HttpStatus.UNAUTHORIZED)
+  @ExceptionHandler(IllegalArgumentException.class)
+  public ErrorDto.Response noAuthorityHandle(NoAuthorityException e) {
     return new ErrorDto.Response(BadRequestException.httpStatus, e.getCode(), e.getErrorMsg());
   }
 }
---
+++ src/main/java/com/four/brothers/runtou/exception/NoAuthorityException.java
@@ -0,0 +1,31 @@
+package com.four.brothers.runtou.exception;
+
+import com.four.brothers.runtou.exception.code.ExceptionCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import javax.naming.AuthenticationException;
+
+@Getter
+public class NoAuthorityException extends AuthenticationException {
+  public static HttpStatus httpStatus = HttpStatus.UNAUTHORIZED;
+
+  private ExceptionCode code;
+  private String errorMsg;
+
+  /**
+   * ExceptionCode 인터페이스를 상속받은 ENUM 타입 처리
+   * @param code
+   */
+  public NoAuthorityException(ExceptionCode code) {
+    if (!code.getClass().isEnum()) {
+      throw new IllegalArgumentException("매개변수로 enum 타입이 와야합니다.");
+    }
+    if (!(code instanceof ExceptionCode)) {
+      throw new IllegalArgumentException("오류코드 enum은 ExceptionCode 인터페이스를 상속받아야 합니다.");
+    }
+
+    this.code = code;
+    this.errorMsg = code.getValue();
+  }
+}
