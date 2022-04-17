5309fdb219afb81973894631ab3d41a5dabf1c4d
TaegyunWoo <dnxprbs@gmail.com>
2022-04-17 18:10:30 +0900

    Feat: 채팅방 DTO 클래스 추가

---
+++ src/main/java/com/four/brothers/runtou/dto/ChatRoomDto.java
@@ -0,0 +1,23 @@
+package com.four.brothers.runtou.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+public class ChatRoomDto {
+  @Getter
+  @Setter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class NewChatRoomRequest {
+    @NotNull
+    private long ordererPk;
+    @NotNull
+    private long performerPk;
+    @NotNull
+    private long orderSheetPk;
+  }
+}
