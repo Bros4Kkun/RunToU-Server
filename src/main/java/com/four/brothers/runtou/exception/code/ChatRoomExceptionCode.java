745eae6e38bb571a95053501dd081dfcf3bb0065
TaegyunWoo <dnxprbs@gmail.com>
2022-04-17 19:25:10 +0900

    Feat: 채팅방 예외 코드 추가

---
+++ src/main/java/com/four/brothers/runtou/exception/code/ChatRoomExceptionCode.java
@@ -0,0 +1,20 @@
+package com.four.brothers.runtou.exception.code;
+
+/**
+ * 채팅방 관련 예외 코드
+ */
+public enum ChatRoomExceptionCode implements ExceptionCode {
+  REQUEST_USER_IS_NOT_PERFORMER("오직 PERFORMER만 채팅을 요청할 수 있습니다."),
+  ALREADY_EXIST_CHAT_ROOM("이미 존재하는 채팅방입니다.");
+
+  private String value;
+
+  ChatRoomExceptionCode(String value) {
+    this.value = value;
+  }
+
+  @Override
+  public String getValue() {
+    return null;
+  }
+}
