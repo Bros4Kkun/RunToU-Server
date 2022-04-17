b88d820c33ceff115c0466c0f53766a93b1ec0f4
TaegyunWoo <dnxprbs@gmail.com>
2022-04-17 20:20:08 +0900

    Fix: 웹소켓 통신 예시 일부 삭제

--- src/main/java/com/four/brothers/runtou/controller/SocketController.java
+++ src/main/java/com/four/brothers/runtou/controller/SocketController.java
@@ -25,14 +25,4 @@ public class SocketController {
     log.info("React to hello-msg-mapping");
     return new EchoDto(message.trim());
   }
-
-  /**
-   * 직접 Service 인스턴스를 사용하여, 웹소켓 통신하기
-   * @param message
-   */
-  @PostMapping("/hello-convert-and-send")
-  public void echoConvertAndSend(@RequestParam("msg") String message) {
-    log.info("echoConvertAndSend");
-    socketService.echoMessage(message);
-  }
 }
