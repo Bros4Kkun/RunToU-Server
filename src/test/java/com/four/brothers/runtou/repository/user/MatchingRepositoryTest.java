652c93ed00fcc5183ad96106ae2fe620b2c86563
ck1411 <ckck1411@gmail.com>
2022-04-19 16:05:33 +0900

    review 사항 수정

--- src/test/java/com/four/brothers/runtou/repository/user/MatchingRepositoryTest.java
+++ src/test/java/com/four/brothers/runtou/repository/user/MatchingRepositoryTest.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Import;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static com.sun.tools.doclint.Entity.or;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Import({MatchingRepository.class, OrdererRepository.class, OrderSheetRepository.class, PerformerRepository.class})
@@ -38,9 +37,9 @@ class MatchingRepositoryTest {
     String phoneNumber = "01021311231";
     String accountNumber = "12341234111";
     Boolean isDoingJobNow = false;
-    Orderer orderer = new Orderer(accountId, password, realname, nickname, phoneNumber, accountNumber, isDoingJobNow);
+
     ordererRepository.saveOrderer(accountId, password, realname, nickname, phoneNumber, accountNumber);
-    Orderer findOrderer = ordererRepository.findAll(1,1).get(0);
+    Orderer orderer = ordererRepository.findAll(1,1).get(0);
 
     String title = "장보기";
     String content = "당근사다주세요";
@@ -49,9 +48,9 @@ class MatchingRepositoryTest {
     Integer cost = 1000;
     Boolean isPayed = true;
     LocalDateTime wishedDeadline = LocalDateTime.now();
-    OrderSheet orderSheet = new OrderSheet(orderer, title, content, category, destination, cost, isPayed, wishedDeadline);
-    orderSheetRepository.saveOrderSheet(findOrderer, title, content, category, destination, cost, isPayed, wishedDeadline);
-    OrderSheet findOrderSheet = orderSheetRepository.findAll(1,1).get(0);
+
+    orderSheetRepository.saveOrderSheet(orderer, title, content, category, destination, cost, isPayed, wishedDeadline);
+    OrderSheet orderSheet = orderSheetRepository.findAll(1,1).get(0);
 
     String accountIdP = "testP";
     String passwordP = "testP";
@@ -73,7 +72,7 @@ class MatchingRepositoryTest {
     //when-then
     assertDoesNotThrow(
         ()->{
-          matchingRepository.saveMatching(findOrderSheet,findPerformer,isCompleted,completedDateTime);
+          matchingRepository.saveMatching(orderSheet, findPerformer, isCompleted, completedDateTime);
         }
     );
 
@@ -148,7 +147,7 @@ class MatchingRepositoryTest {
     Boolean isDoingJobNow = false;
     Orderer orderer = new Orderer(accountId, password, realname, nickname, phoneNumber, accountNumber, isDoingJobNow);
     ordererRepository.saveOrderer(accountId, password, realname, nickname, phoneNumber, accountNumber);
-    Orderer findOrderer = ordererRepository.findAll(1,1).get(0);
+    Orderer orderer1 = ordererRepository.findAll(1,1).get(0);
 
     String title = "장보기";
     String content = "당근사다주세요";
@@ -157,9 +156,9 @@ class MatchingRepositoryTest {
     Integer cost = 1000;
     Boolean isPayed = true;
     LocalDateTime wishedDeadline = LocalDateTime.now();
-    OrderSheet orderSheet = new OrderSheet(orderer, title, content, category, destination, cost, isPayed, wishedDeadline);
-    orderSheetRepository.saveOrderSheet(findOrderer, title, content, category, destination, cost, isPayed, wishedDeadline);
-    OrderSheet findOrderSheet = orderSheetRepository.findAll(1,1).get(0);
+
+    orderSheetRepository.saveOrderSheet(orderer1, title, content, category, destination, cost, isPayed, wishedDeadline);
+    OrderSheet orderSheet = orderSheetRepository.findAll(1,1).get(0);
 
     String accountIdP = "testP";
     String passwordP = "testP";
@@ -173,14 +172,14 @@ class MatchingRepositoryTest {
     Performer performer = new Performer(accountIdP, passwordP, realNameP, nicknameP, phoneNumberP,
         accountNumberP, isDoingJobNowP, becamePerformerDateTimeP, earnedMoney);
     performerRepository.savePerformer(accountIdP, passwordP, realNameP, nicknameP, phoneNumberP, accountNumberP);
-    Performer findPerformer = performerRepository.findAll(1,1).get(0);
+    Performer performer1 = performerRepository.findAll(1,1).get(0);
 
     boolean isCompleted = false;
     LocalDateTime completedDateTime = LocalDateTime.now();
 
     long pk;
 
-    matchingRepository.saveMatching(findOrderSheet,findPerformer,isCompleted,completedDateTime);
+    matchingRepository.saveMatching(orderSheet, performer1, isCompleted, completedDateTime);
     matchingRepository.findAll(1,1);
 
     pk = matchingRepository.findAll(1,1).get(0).getId();
