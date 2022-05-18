547193db751311080906746aaa99d4b29119ce5d
TaegyunWoo <dnxprbs@gmail.com>
2022-05-18 14:51:05 +0900

    Fix: 엔티티 id 필드 채울 수 있는 생성자 추가
    - resolved #96

--- src/main/java/com/four/brothers/runtou/domain/Admin.java
+++ src/main/java/com/four/brothers/runtou/domain/Admin.java
@@ -8,12 +8,15 @@ import javax.persistence.Entity;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 public class Admin extends User {
 
   public Admin(String accountId, String password, String realName, String nickname, String phoneNumber, String accountNumber) {
     super(accountId, password, realName, nickname, phoneNumber, accountNumber);
+  }
+
+  public Admin(long id, String accountId, String password, String realName, String nickname, String phoneNumber, String accountNumber) {
+    super(id, accountId, password, realName, nickname, phoneNumber, accountNumber);
   }
 
 }
