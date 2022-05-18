7c5523ec250b166cee49156c4b1e15749f0c9cad
TaegyunWoo <dnxprbs@gmail.com>
2022-05-18 16:15:31 +0900

    Feat: 포인트 관련 예외 코드 수정
    - 필요없는 enum 삭제
    - 필요한 enum 추가

--- src/main/java/com/four/brothers/runtou/exception/code/PointExceptionCode.java
+++ src/main/java/com/four/brothers/runtou/exception/code/PointExceptionCode.java
@@ -4,7 +4,7 @@ package com.four.brothers.runtou.exception.code;
  * 포인트 관련 예외 코드
  */
 public enum PointExceptionCode implements ExceptionCode {
-  PERFORMER_CANNOT_CHARGE_POINT("수행자가 포인트 충전을 시도함.");
+  CANNOT_USE_POINT("포인트를 사용할 수 없음.");
 
   private String value;
 
