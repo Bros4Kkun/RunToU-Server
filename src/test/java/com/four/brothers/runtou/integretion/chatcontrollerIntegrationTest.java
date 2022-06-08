56ffab525e98215ac0f6641d169d19a7750de46a
ck1411 <ckck1411@gmail.com>
2022-06-09 01:45:48 +0900

    Test: ChatControllerIntegrationTest done

---
+++ src/test/java/com/four/brothers/runtou/integretion/chatcontrollerIntegrationTest.java
@@ -0,0 +1,145 @@
+package com.four.brothers.runtou.integretion;
+
+import com.four.brothers.runtou.domain.ChatMessage;
+import com.four.brothers.runtou.dto.*;
+import com.four.brothers.runtou.repository.ChatMessageRepository;
+import com.four.brothers.runtou.service.ChatService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(
+    properties = {
+        "spring.config.location=classpath:application-local.properties",
+        "spring.config.location=classpath:application.properties"
+    },
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+
+public class chatcontrollerIntegrationTest {
+
+  @Autowired
+  private TestRestTemplate template;
+
+  /**
+   * 공용으로 사용하기 위해, 따로 빼둔 '요청자 회원가입 및 로그인 메서드'
+   * @return JSESSIONID
+   */
+  private String signUpAndLoginAsOrderer() {
+    //GIVEN
+    String accountId = "testAccountId";
+    String realName = "testRealName";
+    String nickName = "testnickname";
+    String passWord = "password";
+    String phoneNumber = "01011112222";
+    String accountNumber = "1231231231313";
+    String rawPassword = "password";
+    UserRole role = UserRole.ORDERER;
+
+    OrdererDto.SignUpAsOrdererRequest request = new OrdererDto.SignUpAsOrdererRequest();
+
+    request.setAccountId(accountId);
+    request.setRealName(realName);
+    request.setNickname(nickName);
+    request.setPassword(passWord);
+    request.setPhoneNumber(phoneNumber);
+    request.setAccountNumber(accountNumber);
+
+    LoginDto.LoginRequest request1 = new LoginDto.LoginRequest();
+
+    request1.setAccountId(accountId);
+    request1.setRawPassword(rawPassword);
+    request1.setRole(role);
+
+    //WHEN
+    HttpStatus resultStatusCode = template.postForEntity("/api/user/signup/orderer", request, OrdererDto.SignUpAsOrdererResponse.class)
+        .getStatusCode();
+    HttpStatus resultStatusCode1 = template.postForEntity(
+        "/api/user/signin", request1, LoginDto.LoginResponse.class).getStatusCode();
+    //THEN
+
+    List<String> strings = template.postForEntity(
+        "/api/user/signin", request1, LoginDto.LoginResponse.class).getHeaders().get("Set-Cookie");
+
+    return strings.get(0);
+  }
+/*
+  @Transactional
+  @DisplayName("메세지보내기")
+  @Test
+  void sendChatTest(){
+    //GIVEN
+    String msg = "hi";
+    long chatRoomPk = 33;
+    String jsessionid = signUpAndLoginAsOrderer();
+    long userpk = 22;
+    String accountId = "test";
+    String realName = "testre";
+    String nickname = "testni";
+    String phoneNumber = "0000001101";
+    String accountNumber = "22223241414";
+    UserRole role = UserRole.ORDERER;
+
+    LoginDto.LoginUser loginUser = new LoginDto.LoginUser();
+    loginUser.setUserPk(userpk);
+    loginUser.setAccountId(accountId);
+    loginUser.setRealName(realName);
+    loginUser.setNickname(nickname);
+    loginUser.setPhoneNumber(phoneNumber);
+    loginUser.setAccountNumber(accountNumber);
+    loginUser.setRole(role);
+
+    //ChatMessageDto.ChatMessageResponse response = new ChatMessageDto.ChatMessageResponse();
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("Cookie", jsessionid);
+    headers.set("Content-Type", "application/json");
+
+    HttpEntity requestHttpEntity = new HttpEntity(msg, headers);
+
+    ResponseEntity<String> responseHttpEntity = template.exchange(
+        "/api/chat/chatroom/" + chatRoomPk,
+        HttpMethod.POST,
+        requestHttpEntity,
+        String.class
+    );
+    System.out.println(responseHttpEntity);
+    //THEN
+    assertEquals(true,responseHttpEntity.getStatusCode().is2xxSuccessful());
+
+  }*/
+
+  @Transactional
+  @DisplayName("jwt토큰발급")
+  @Test
+  void getJwtTokenTest()
+  {
+    //GIVEN
+    String jsessionid = signUpAndLoginAsOrderer();
+    String token = "aaaaa";
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("Cookie", jsessionid);
+    headers.set("Content-Type", "application/json");
+
+    HttpEntity requestHttpEntity = new HttpEntity(token, headers);
+
+    ResponseEntity<String> responseHttpEntity = template.exchange(
+        "/api/chat/jwt",
+        HttpMethod.GET,
+        requestHttpEntity,
+        String.class
+    );
+
+    //THEN
+    assertEquals(true, responseHttpEntity.getStatusCode().is2xxSuccessful());
+  }
+}
